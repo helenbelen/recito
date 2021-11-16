eb3ccb3429712b648189ff3ab025551b2cb89892
HelenAgha <helen.agha@target.com>
2021-11-16 09:38:19 -0600

    add google books api call

--- main.js
+++ main.js
@@ -1,17 +1,33 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, net, ipcMain} = require('electron')
 const path = require('path')
+const https = require("https");
 
 function createWindow () {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js')
+            preload: path.join(__dirname, 'preload.js'),
+            contextIsolation: true,
+            sandbox: false
         }
     })
 
     win.loadFile('index.html')
 }
+
+async function queryBooks (event, term) {
+    await https.get(`https://www.googleapis.com/books/v1/volumes?q=intitle:${term}&key=${process.env.API_KEY}`, (response) => {
+        response.on('data', chunk => {
+            event.reply('sendBooksApiResponse', chunk.toString())
+        })
+    })
+}
+
+ipcMain.on('runBooksApi', (event, arg) => {
+    queryBooks(event, arg).then(r => console.log(`search for '${arg}' complete`))
+})
+
 app.whenReady().then(() => {
     createWindow()
     app.on('activate', function () {
--- preload.js
+++ preload.js
@@ -1,3 +1,32 @@
+const {contextBridge, ipcRenderer} = require('electron')
+const Buffer = require("buffer");
+
+contextBridge.exposeInMainWorld(
+    'electron',
+    {
+        runBooks: (searchTerm) => ipcRenderer.send('runBooksApi', searchTerm),
+        myPromises: [Promise.resolve(), Promise.reject(new Error('whoops'))],
+        anAsyncFunction: async () => alert(123),
+        data: {
+            myFlags: ['a', 'b', 'c'],
+            bootTime: 1234
+        },
+        nestedAPI: {
+            evenDeeper: {
+                youCanDoThisAsMuchAsYouWant: {
+                    fn: () => ({
+                        returnData: 123
+                    })
+                }
+            }
+        }
+    }
+)
+
+ipcRenderer.on('sendBooksApiResponse', (event, arg) => {
+    document.getElementById('results').innerText = arg
+})
+
 window.addEventListener('DOMContentLoaded', () => {
     const replaceText = (selector, text) => {
         const element = document.getElementById(selector)
